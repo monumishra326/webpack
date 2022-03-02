c714be4e28bc0639870a7d86918edbb9fe35156f
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-02 23:27:34 +0530

    webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function add(firstname,lastname) {
+    return firstname+lastname;
+}
+
+
+console.log(add(5,5));
+import "./index.css"
+
+const h1=document.createElement("h1");
+h1.textContent="Hello Monu Mishra";
+document.getElementById("root").appendChild(h1);
+h1.classList.add("red");
+
